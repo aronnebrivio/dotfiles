87bf4260ff9ea0d7985a6ace0c21b2d91c5eb623
Aronne Brivio <aroblu94@gmail.com>
2020-03-28 11:37:57 +0100

    Test

--- config/fish/functions/android23emu.fish
+++
@@ -1,5 +0,0 @@
-function android23emu
-	cd /home/aronne/Android/Sdk/emulator/
-./emulator -avd Nexus_5X_API_25 &
-cd -
-end
--- config/fish/functions/fish_prompt.fish
+++
@@ -1,1 +0,0 @@
-/home/aronne/.local/share/omf/themes/bobthefish/fish_prompt.fish
