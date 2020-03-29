ce823bd10f6540fa4572d120550dc56573c68fda
Aronne Brivio <aronne.brivio@gmail.com>
2020-03-29 23:38:49 +0200

    Moved fisher to conf.d

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -57,13 +57,6 @@ set -g theme_nerd_fonts yes
 # Colorls
 # source (dirname (gem which colorls))/tab_complete.sh
 
-# Fisher
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 #
 ### THEME
 #
