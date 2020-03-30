f2abce1afb00da3108e59188cca1a2d4748c2f4a
Aronne Brivio <aronne.brivio@gmail.com>
2020-03-30 10:29:14 +0200

    Update gitconfig. Moved fisher back to config.fish to avoid file deletion in conf.d/

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -51,6 +51,13 @@ if [ -f $HOME/.config/fish/omf.fish ]
     source $HOME/.config/fish/omf.fish
 end
 
+# Fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # Enable terminal Nerd Fonts
 set -g theme_nerd_fonts yes
 
