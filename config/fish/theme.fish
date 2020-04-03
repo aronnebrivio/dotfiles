e411dd30f72c18a66be860dc85109e4454c27fa9
Aronne Brivio <aronne.brivio@gmail.com>
2020-04-04 01:43:17 +0200

    Update theme

--- config/fish/theme.fish
+++ config/fish/theme.fish
@@ -7,3 +7,8 @@ set -g theme_display_git_master_branch yes
 set -g theme_color_scheme base16-dark
 set -g theme_title_display_process yes
 set -g theme_title_use_abbreviated_path yes
+
+#
+### base16 theme
+#
+base16-monokai
