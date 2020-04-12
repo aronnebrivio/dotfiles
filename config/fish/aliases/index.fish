993b5046881f21bf4e0dd0a09a4ab1a654d80537
Aronne Brivio <aronne.brivio@gmail.com>
2020-04-12 16:49:36 +0200

    Handling alias overrides

--- config/fish/aliases/index.fish
+++ config/fish/aliases/index.fish
@@ -2,6 +2,11 @@
 
 # (dirname (status -f)) give the current script directory
 # see https://github.com/fish-shell/fish-shell/issues/1818
-for f in (ls (dirname (status -f))/*.fish | grep -v "index.fish")
+for f in (ls (dirname (status -f))/*.fish | grep -v "[index|local].fish"
     source $f
+end
+
+# Local add/overrides
+if [ -f $HOME/.config/fish/aliases/local.fish ]
+    source $HOME/.config/fish/aliases/local.fish
 end
