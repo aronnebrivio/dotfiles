79c83b9b9e8c4866460c5e3221d96317f3c86124
Aronne Brivio <aronne.brivio@gmail.com>
2020-09-01 15:14:40 +0200

    Update to portainer-ce

--- config/fish/functions/startPortainer.fish
+++ config/fish/functions/startPortainer.fish
@@ -4,13 +4,16 @@ function startPortainer -a port -d "Start Portainer in a Docker container expose
         exit 1
     end
 
+    # Remove old container
+    docker rm --force portainer
+
     # Ensures it always uses the latest version
-    docker pull portainer/portainer
+    docker pull portainer/portainer-ce
 
     # Fallback to default port if not set
     set -q port[1]; or set port 9999
 
     # Run Docker container
-    docker run --name portainer --restart=unless-stopped -d -p $port:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+    docker run --name portainer --restart=unless-stopped -d -p $port:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 end
