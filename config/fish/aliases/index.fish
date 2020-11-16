fd58298a99a18a37915dbd0715b8dfefe932e43e
Aronne Brivio <aronne.brivio@gmail.com>
2020-11-17 00:13:39 +0100

    Fix aliases naming

--- config/fish/aliases/index.fish
+++ config/fish/aliases/index.fish
@@ -2,7 +2,7 @@
 
 # (dirname (status -f)) give the current script directory
 # see https://github.com/fish-shell/fish-shell/issues/1818
-for f in (/bin/ls (dirname (status -f))/*.fish | grep -v "[index|local].fish")
+for f in (/bin/ls (dirname (status -f))/*.fish | grep -Ev "index|local")
     source $f
 end
 
